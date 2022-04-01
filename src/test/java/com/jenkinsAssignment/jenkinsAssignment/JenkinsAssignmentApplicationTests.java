7f0854b04c3fb09345dd48d4407a06767a418e8d
devopsbymat <devops.bymat@gmail.com>
2022-04-01 18:04:22 +0530

    initial commit

---
+++ src/test/java/com/jenkinsAssignment/jenkinsAssignment/JenkinsAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenkinsAssignment.jenkinsAssignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

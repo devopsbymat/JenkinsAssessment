aeaf6b303482a06f229334f3bf5dd0e03e7bb271
devopsbymat <devops.bymat@gmail.com>
2022-04-02 16:46:09 +0530

    updated files

--- src/main/java/com/jenkinsAssignment/JenkinsAssignmentApplication.java
+++ src/main/java/com/jenkinsAssignment/JenkinsAssignmentApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JenkinsAssignmentApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(JenkinsAssignmentApplication.class, args);
+
 	}
 
 }
